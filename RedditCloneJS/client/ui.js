104f55d0fe3ec639cd88728d1e6d93e05de7bd1b
TheoWinter <theo.winter@gmail.com>
2014-11-26 13:05:26 +0100

    sync

--- RedditCloneJS/client/ui.js
+++ RedditCloneJS/client/ui.js
@@ -2,7 +2,22 @@
 window.onload = function () {
     "use strict";
     document.getElementById("loginButton").onclick = function () {
-        alert('Hello World');
+        alert('Hello ' + document.getElementById("username").value);
+        return false;
+    };
+
+    document.getElementById("submitNewLink").onclick = function () {
+        alert('Submitting new links is currently not supported');
+        return false;
+    };
+
+    document.getElementById("submitNewTextPost").onclick = function () {
+        alert('Submitting new text posts is currently not supported');
+        return false;
+    };
+
+    document.getElementById("createNewSubreddit").onclick = function () {
+        alert('Creating new subreddits is currently not supported');
         return false;
     };
 }
